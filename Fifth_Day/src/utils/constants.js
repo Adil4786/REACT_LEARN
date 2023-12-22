71867b02a11a37cc062a7b874d24b43fb2f3c4cb
Adil <Mohemmed.Adil@t-systems.com>
2023-12-22 23:41:39 +0530

    Fifth Day Learning

---
+++ Fifth_Day/src/utils/constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL = 
+    "https://i.fbcd.co/products/resized/resized-750-500/c5deb3e7b3c3d6f8397953a112dfda453703fe73a21ccbea43bde8f912fa3aae.jpg";
